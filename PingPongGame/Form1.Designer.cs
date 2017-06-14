e714385cf16d9343ccb6ce3da100c100f7a6bb9b
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-06-14 23:47:41 +0200

    Added most of the features i wanted to implement

--- PingPongGame/Form1.Designer.cs
+++ PingPongGame/Form1.Designer.cs
@@ -29,11 +29,162 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.movementTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2Score = new System.Windows.Forms.Label();
+            this.pressStartToPlay = new System.Windows.Forms.Label();
+            this.Pirate = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pirate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // movementTimer
+            // 
+            this.movementTimer.Enabled = true;
+            this.movementTimer.Interval = 20;
+            this.movementTimer.Tick += new System.EventHandler(this.movementTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(251, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Player1 Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(440, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Player2 Score:";
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.player1Score.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.player1Score.Location = new System.Drawing.Point(266, 29);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(0, 20);
+            this.player1Score.TabIndex = 5;
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.player2Score.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.player2Score.Location = new System.Drawing.Point(451, 29);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(0, 20);
+            this.player2Score.TabIndex = 6;
+            // 
+            // pressStartToPlay
+            // 
+            this.pressStartToPlay.AutoSize = true;
+            this.pressStartToPlay.Font = new System.Drawing.Font("Palatino Linotype", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pressStartToPlay.Location = new System.Drawing.Point(276, 191);
+            this.pressStartToPlay.Name = "pressStartToPlay";
+            this.pressStartToPlay.Size = new System.Drawing.Size(258, 32);
+            this.pressStartToPlay.TabIndex = 7;
+            this.pressStartToPlay.Text = "Press stHARRt to play";
+            // 
+            // Pirate
+            // 
+            this.Pirate.Image = global::PingPongGame.Properties.Resources.HAR;
+            this.Pirate.Location = new System.Drawing.Point(313, 54);
+            this.Pirate.Name = "Pirate";
+            this.Pirate.Size = new System.Drawing.Size(153, 134);
+            this.Pirate.TabIndex = 8;
+            this.Pirate.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.Image = global::PingPongGame.Properties.Resources.ball;
+            this.ball.Location = new System.Drawing.Point(390, 227);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(15, 15);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.Transparent;
+            this.player1.Image = global::PingPongGame.Properties.Resources.pirateShip2;
+            this.player1.Location = new System.Drawing.Point(-24, 158);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(65, 105);
+            this.player1.TabIndex = 2;
+            this.player1.TabStop = false;
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.Color.Black;
+            this.player2.Image = global::PingPongGame.Properties.Resources.pirateShip;
+            this.player2.Location = new System.Drawing.Point(706, 158);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(65, 105);
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(84)))));
+            this.ClientSize = new System.Drawing.Size(759, 413);
+            this.Controls.Add(this.Pirate);
+            this.Controls.Add(this.pressStartToPlay);
+            this.Controls.Add(this.player2Score);
+            this.Controls.Add(this.player1Score);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.player2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HARR-pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Player_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Player_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Pirate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Timer movementTimer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Label player2Score;
+        private System.Windows.Forms.Label pressStartToPlay;
+        private System.Windows.Forms.PictureBox Pirate;
     }
 }
 
--- PingPongGame/Form1.cs
+++ PingPongGame/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PingPongGame
@@ -15,6 +9,189 @@ namespace PingPongGame
         public Form1()
         {
             InitializeComponent();
+        }
+
+        bool gameOn = false;
+        int speedX = -10;
+        int speedY = -10;
+        int player1Speed = 10;
+        int player2Speed = 10;
+        bool player1Up, player2Up, player1Down, player2Down = false;
+
+        private void movementTimer_Tick(object sender, EventArgs e)
+        {
+            if (player1Score.Text == "xxxxx" || player2Score.Text == "xxxxx")
+            {
+                gameOn = false;
+                pressStartToPlay.Text = (player1Score.Text == "xxxxx") ? "Winner: Player1" : "Winner: Player2";
+                pressStartToPlay.Visible = true;
+            }
+                
+            if (gameOn)
+            {
+                moveBall();
+                if (player1Up && !Collision_Top(player2))
+                {
+                    player2.Top -= player1Speed;
+                }
+                if (player1Down && !Collision_Bottom(player2))
+                {
+                    player2.Top += player1Speed;
+                }
+                if (player2Up && !Collision_Top(player1))
+                {
+                    player1.Top -= player2Speed;
+                }
+                if (player2Down && !Collision_Bottom(player1))
+                {
+                    player1.Top += player2Speed;
+                }
+            }
+        }
+
+        private void Player_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.O:
+                    player1Down = false;
+                    player1Up = true;
+                    break;
+                case Keys.L:
+                    player1Up = false;
+                    player1Down = true;
+                    break;
+                case Keys.W:
+                    player2Up = true;
+                    player2Down = false;
+                    break;
+                case Keys.S:
+                    player2Up = false;
+                    player2Down = true;
+                    break;
+                case Keys.Space:
+                    gameOn = true;
+                    pressStartToPlay.Visible = false;
+                    Pirate.Visible = false;
+                    break;
+                case Keys.Escape:
+                    gameOn = false;
+                    pressStartToPlay.Text = "Paused";
+                    pressStartToPlay.Visible = true;
+                    break;
+            }
+        }
+
+        private void Player_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.O:
+                    player1Up = false;
+                    break;
+                case Keys.W:
+                    player2Up = false;
+                    break;
+                case Keys.L:
+                    player1Down = false;
+                    break;
+                case Keys.S:
+                    player2Down = false;
+                    break;
+            }
+        }
+
+        public void moveBall()
+        {
+            
+            if (Collision_Right(ball))
+            {
+                speedX *= -1;
+                speedY *= 1;
+                player1Score.Text += "x";
+            }
+                
+            if (Collision_Left(ball))
+            {
+                speedX *= -1;
+                speedY *= 1;
+                player2Score.Text += "x";
+            }
+                
+            if (Collision_Top(ball))
+            {
+                speedX *= 1;
+                speedY *= -1;
+            }
+                
+            if (Collision_Bottom(ball))
+            {
+                speedX *= 1;
+                speedY *= -1;
+            }
+            if (Collision_Player1(ball))
+            {
+                speedX *= -1;
+                speedY *= 1;
+            }
+            if (Collision_Player2(ball))
+            {
+                speedX *= -1;
+                speedY *= 1;
+            }
+
+            int ballx = ball.Location.X;
+            int bally = ball.Location.Y;
+            ball.Location = new Point(ballx + speedX, bally + speedY);
+
+            
+
+        }
+
+        public Boolean Collision_Left(PictureBox obj)
+        {
+            if (obj.Location.X <= 0)
+                return true;
+            return false;
+        }
+
+        public Boolean Collision_Right(PictureBox obj)
+        {
+            if (obj.Location.X >= 755)
+                return true;
+            return false;
+        }
+        public Boolean Collision_Top(PictureBox obj)
+        {
+            if (obj.Location.Y <= 0)
+                return true;
+            return false;
+        }
+        public Boolean Collision_Bottom(PictureBox obj)
+        {
+            if (obj.Location.Y + obj.Height >= 410)
+                return true;
+            return false;
+        }
+
+
+
+        public bool Collision_Player1(PictureBox ball)
+        {
+            if (player1.Bounds.IntersectsWith(ball.Bounds))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Collision_Player2(PictureBox ball)
+        {
+            if (player2.Bounds.IntersectsWith(ball.Bounds))
+            {
+                Collision_Left(ball);
+                return true;
+            }
+            return false;
         }
     }
 }
