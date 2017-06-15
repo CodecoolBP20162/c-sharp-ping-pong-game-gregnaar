e76f71e656a81850f5863bcebe3d271c059ca94c
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-06-15 10:38:44 +0200

    rerrolled some changes (killed the local repo :O)

--- PingPongGame/Form1.Designer.cs
+++ PingPongGame/Form1.Designer.cs
@@ -150,6 +150,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(84)))));
+            this.BackgroundImage = global::PingPongGame.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(759, 413);
             this.Controls.Add(this.Pirate);
             this.Controls.Add(this.pressStartToPlay);
